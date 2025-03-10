860897ed50c59fd5504971f309476d286dbfe9e9
raphaelzolotarev <raphaelzolotarev@gmail.com>
2025-03-10 06:06:26 +0100

    User update image profile

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -78,4 +78,68 @@ export class ProfileComponent implements OnInit {
     }
   }
 
+  updateAccountSettings(settingsForm: NgForm): void {
+    this.isLoadingSubject.next(true);
+    this.profileState$ = this.userService.updateAccountSettings$(settingsForm.value)
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next({ ...response, data: response.data });
+          this.isLoadingSubject.next(false);
+          return { dataState: DataState.LOADED, appData: this.dataSubject.value };
+        }),
+        startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.isLoadingSubject.next(false);
+          return of({ dataState: DataState.LOADED, appData: this.dataSubject.value, error })
+        })
+      )
+  }
+
+  toggleMfa(): void {
+    this.isLoadingSubject.next(true);
+    this.profileState$ = this.userService.toggleMfa$()
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next({ ...response, data: response.data });
+          this.isLoadingSubject.next(false);
+          return { dataState: DataState.LOADED, appData: this.dataSubject.value };
+        }),
+        startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.isLoadingSubject.next(false);
+          return of({ dataState: DataState.LOADED, appData: this.dataSubject.value, error })
+        })
+      )
+  }
+
+  updatePicture(image: File): void {
+    if (image) {
+      this.isLoadingSubject.next(true);
+      this.profileState$ = this.userService.updateImage$(this.getFormData(image))
+        .pipe(
+          map(response => {
+            console.log(response);
+            this.dataSubject.next({ ...response, 
+              data: { ...response.data, 
+                user: { ...response.data.user, picture: `${response.data.user.picture}?time=${new Date().getTime()}`}} });
+            this.isLoadingSubject.next(false);
+            return { dataState: DataState.LOADED, appData: this.dataSubject.value };
+          }),
+          startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
+          catchError((error: string) => {
+            this.isLoadingSubject.next(false);
+            return of({ dataState: DataState.LOADED, appData: this.dataSubject.value, error })
+          })
+        )
+    }
+  }
+
+  private getFormData(image: File): FormData {
+    const formData = new FormData();
+    formData.append('image', image);
+    return formData;
+  }
+
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -61,10 +61,34 @@ export class UserService {
           }),
           catchError(this.handleError)
         );
-        
+
         updatePassword$ = (form: { currentPassword: string, newPassword: string, confirmNewPassword: string }) => <Observable<CustomHttpResponse<Profile>>>
         this.http.patch<CustomHttpResponse<Profile>>
           (`${this.server}/user/update/password`, form)
+          .pipe(
+            tap(console.log),
+            catchError(this.handleError)
+          );
+
+  updateAccountSettings$ = (settings: { enabled: boolean, notLocked: boolean }) => <Observable<CustomHttpResponse<Profile>>>
+    this.http.patch<CustomHttpResponse<Profile>>
+      (`${this.server}/user/update/settings`, settings)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+      toggleMfa$ = () => <Observable<CustomHttpResponse<Profile>>>
+        this.http.patch<CustomHttpResponse<Profile>>
+          (`${this.server}/user/togglemfa`, {})
+          .pipe(
+            tap(console.log),
+            catchError(this.handleError)
+          );
+    
+      updateImage$ = (formData: FormData) => <Observable<CustomHttpResponse<Profile>>>
+        this.http.patch<CustomHttpResponse<Profile>>
+          (`${this.server}/user/update/image`, formData)
           .pipe(
             tap(console.log),
             catchError(this.handleError)
